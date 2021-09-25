e3008a86278641a12b8cadf09ec39df6405d3c37
CNbysec <547968300@qq.com>
2021-09-26 00:34:53 +0800

    main

--- OSS/js/main.js
+++ OSS/js/main.js
@@ -60,8 +60,7 @@ class Viewer {
             if (this.model) {
                 this.model.position = new PIXI.Point((width * 0.5), (height * 0.5));
                 this.model.scale = new PIXI.Point((this.model.position.x * 0.06), (this.model.position.x * 0.06));
-                this.model.masks.resize(this.app.view.width, this.app.view.height);
-
+                //this.model.masks.resize(this.app.view.width, this.app.view.height);
             }
 
         };
